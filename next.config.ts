3e1b306abfce24ee714a90e8b30e6d888e11f62a
SUFIKX 911 <sabersiso0554@gmail.com>
2025-12-13 09:11:22 +0000

    ضيف لي صفحه جديده باسم ladybet صفحه مثل صفحه nasserbets لكن بالوان ورديه

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,12 @@ const nextConfig: NextConfig = {
         hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
