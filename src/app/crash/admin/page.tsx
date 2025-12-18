e11325b799fe66fccb30dad0af51859987ae4a44
SUFIKX 911 <sabersiso0554@gmail.com>
2025-12-18 19:34:41 +0000

    يا حبيبي هل انت حمار؟ قم بحذف. صفحه. crash بالكامل انا لا احتاجه. يعني م

--- src/app/crash/admin/page.tsx
+++ src/app/crash/admin/page.tsx
@@ -226,3 +226,5 @@ export default function CrashAdmin() {
         </div>
     );
 }
+
+    
--- src/app/crash/aze1/page.tsx
+++ src/app/crash/aze1/page.tsx
@@ -5,3 +5,5 @@ import ControlPage from '@/components/control-page';
 export default function CrashControlPage() {
     return <ControlPage pageName="crash" />;
 }
+
+    
